b87fc87a1f6b728b419d5f26bb0df99afc3dd62f
Shraddha Shetty <Shraddha.Shetty@in.ey.com>
2023-02-20 15:20:01 +0530

    DotnetAssignments

---
+++ AccessModifiers_Private/AccessModifiers_Private/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers_Private
+{
+    class ValuesOfVariables
+    {
+        private int x;
+        private int y;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ValuesOfVariables p = new ValuesOfVariables();
+            // Direct access to public members.
+           // p.x = 10; //error- as it is private
+            //p.y = 15; // error -as it is private
+           
+        }
+    }
+}
