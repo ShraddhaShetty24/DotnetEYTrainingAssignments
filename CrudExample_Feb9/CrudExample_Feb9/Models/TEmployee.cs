71a2fb03243f4f88f3ab0fc7d0880718a329bd30
Shraddha Shetty <Shraddha.Shetty@in.ey.com>
2023-02-26 22:43:56 +0530

    Practice

---
+++ CrudExample_Feb9/CrudExample_Feb9/Models/TEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudExample_Feb9.Models
+{
+    public partial class TEmployee
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public int? EmployeeSalary { get; set; }
+        public int? EmployeeAge { get; set; }
+    }
+}
